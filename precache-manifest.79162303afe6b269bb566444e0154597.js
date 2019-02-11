2865d1287e01be565720e894d0a6a5b14abe7b51
Gerome <gerome@arosoftware.com>
2019-02-11 08:59:47 +0800

    deploy

---
+++ precache-manifest.79162303afe6b269bb566444e0154597.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cooking-recipes-rom/robots.txt"
+  },
+  {
+    "revision": "9a3870baf2b4cbbfc1af",
+    "url": "/cooking-recipes-rom/js/chunk-vendors.ecc89fe7.js"
+  },
+  {
+    "revision": "a0353dedd29147904aa6",
+    "url": "/cooking-recipes-rom/js/app.42df00db.js"
+  },
+  {
+    "revision": "8ddf0549f0324fd92e15df6f86ecf1ed",
+    "url": "/cooking-recipes-rom/index.html"
+  },
+  {
+    "revision": "e0ee4bf6303adac3b6ed025354412f33",
+    "url": "/cooking-recipes-rom/img/fb-share-image.jpg"
+  },
+  {
+    "revision": "9a3870baf2b4cbbfc1af",
+    "url": "/cooking-recipes-rom/css/chunk-vendors.a8f2b066.css"
+  },
+  {
+    "revision": "a0353dedd29147904aa6",
+    "url": "/cooking-recipes-rom/css/app.75e3413d.css"
+  }
+];
